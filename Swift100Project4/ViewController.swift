03e4fdd859f666ea668c284138189afdc67e5571
Jay A <justynadusei@gmail.com>
2020-04-02 11:52:35 +0000

    Initial commit

--- Swift100Project4/ViewController.swift
+++ Swift100Project4/ViewController.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2020 Jay A. All rights reserved.
 //
 
-import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view.webView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
