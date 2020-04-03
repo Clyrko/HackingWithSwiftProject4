bab84f9a8eca02c3927792c33a0cec65caf234f2
Jay A <justynadusei@gmail.com>
2020-04-03 12:30:50 +0000

    Added action sheet

--- Swift100Project4/ViewController.swift
+++ Swift100Project4/ViewController.swift
@@ -23,12 +23,22 @@ class ViewController: UIViewController, WKNavigationDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTouched))
+        
         let url = URL(string: "https://twitter.com/Nucleme")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
     }
 
+    @objc func openTouched() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
 
 }
 
